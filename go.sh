cd378af648480d702732a0cb4a32eb1a57ae6a9a
F_acm M_acm L_acm <acm@morbius.mscs.mu.edu>
2018-04-07 22:09:42 -0500

    changed some modes

--- go.sh
+++ go.sh
@@ -1,2 +1,2 @@
-sh compile.sh && echo "running..." && cd bin/ && java scratchreferee/app/Competition $@
+sh compile.sh && echo "running..." && cd bin/ && java -cp ".:../lib/commons-io-2.5/commons-io-2.5.jar:../lib/JSON-java/:../lib/commons-csv-1.5/commons-csv-1.5/jar:../lib/commons-codec-1.10/commons-codec-1.10.jar" scratchreferee/app/Competition $@
 
--- src/scratchreferee/app/Competition.java
+++ src/scratchreferee/app/Competition.java
@@ -26,12 +26,14 @@ public class Competition {
 		}
 		
 		final String webPath = args[1];
-		final String uploadsPath = webPath + "uploads";
+		final String uploadsPath = webPath + "/uploads";
 		final Pattern uploadPattern = Pattern.compile("team([0-9]+)prob([0-9]+)(project|design).+(\\..+)");
 		final Pattern clarifyPattern = Pattern.compile("question.*\\.txt");
 		final String appdataPath = args[0];
 		final String savePath = appdataPath + "/data/submissions";
 		final Pattern savePattern = Pattern.compile("prob([0-9]+)sub([0-9]+).+");
+		System.out.println("Uploads path: " + uploadsPath);
+		System.out.println("Save path: " + savePath);
 
 		final String username = "MUScratchJudging17";
 		final String password = "Dr.Gorliss";
